db1d21f882a076bf9345b39c51bdb6cfb545a6c1
Alexandre Ferreira Benevides <alexandre.ferreira.benevides@gmail.com>
2023-03-25 00:32:19 -0400

    [WIP] repo does not see the manifest created for it....need to investigae that
    Signed-off-by: Alexandre Ferreira Benevides <alexandre.ferreira.benevides@gmail.com>

--- script/manifest/update_manifest_local_dev.sh
+++ script/manifest/update_manifest_local_dev.sh
@@ -1,16 +1,19 @@
 #!/usr/bin/env bash
 
-
 . ./env_var.sh
 
-
-cp ${EL_MANIFEST_DEV}  ./manifest/version_asked.dev.xml
+cp ${EL_MANIFEST_DEV} ./manifest/version_asked.dev.xml
 if [ "$#" -ge 1 ]; then
   # There is a first argument so we want another version of odoo then the default 12
   ERPLIBRE_ODOO_VERSION=${1} # Set a default value of 12 if no parameter is passed
-  sed -i "s/\(revision=\"[^\".]*\.\)\(12\)\([^\".]*\"\)/\1$ERPLIBRE_ODOO_VERSION\3/g" "./manifest/version_asked.dev.xml"
+  if [[ "$(uname)" == "Darwin" ]]; then
+    # macOS
+    sed -i "" "s/\(revision=\"[^\".]*\.\)\(12\)\([^\".]*\"\)/\1${ERPLIBRE_ODOO_VERSION}\3/g" "./manifest/version_asked.dev.xml"
+  else
+    # Linux
+    sed -i "s/\(revision=\"[^\".]*\.\)\(12\)\([^\".]*\"\)/\1${ERPLIBRE_ODOO_VERSION}\3/g" "./manifest/version_asked.dev.xml"
+  fi
 fi
-
 
 source .venv/bin/activate
 #EL_MANIFEST_PROD="./default.xml"
@@ -20,7 +23,10 @@ source .venv/bin/activate
 git daemon --base-path=. --export-all --reuseaddr --informative-errors --verbose &
 DAEMON_PID=$!
 
-./.venv/repo init -u git://127.0.0.1:9418/ -b $(git rev-parse --verify HEAD) -m ./manifest/version_asked.dev.xml
-./.venv/repo sync -v --force-sync -m ./manifest/version_asked.dev.xml
+  echo "Execution de repo"
+./.venv/repo init -u git://127.0.0.1:9418/ -b $(git rev-parse --verify HEAD) -m "./manifest/version_asked.dev.xml"
+  echo "Synch de repo"
+
+./.venv/repo sync -v --force-sync -m "./manifest/version_asked.dev.xml"
 
 kill ${DAEMON_PID}
